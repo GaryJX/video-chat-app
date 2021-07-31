a164444787b3b7ed2ab5d80b3bfeb1e5b929c800
Gary Xie <garyxie13@live.com>
2021-07-31 19:40:48 -0400

    Update UI demo

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,17 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <h1>{{ title }}</h1>
+  <input type="text" ref="name" />
+  <button @click="handleClick">Click me</button>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: "App",
+  data() {
+    return {
+      title: "My First Vue App",
+    };
+  },
 });
 </script>
 
