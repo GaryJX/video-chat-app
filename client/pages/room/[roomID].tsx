26474fcdbe53e964254fe46746a42062b1e24304
Gary Xie <garyxie13@live.com>
2021-08-01 19:27:51 -0400

    Request camera and microphone access

--- client/pages/room/[roomID].tsx
+++ client/pages/room/[roomID].tsx
@@ -1,15 +1,39 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import { useToast } from '@chakra-ui/react'
 import { PuffLoader } from 'react-spinners'
 import colors from 'tailwindcss/colors'
 
 const VideoRoomPage: React.FC = () => {
   const [loading, setLoading] = useState(true)
+  const toast = useToast()
+
+  useEffect(() => {
+    getVideoAndAudioPermissions()
+  }, [])
+
+  const getVideoAndAudioPermissions = async () => {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({
+        video: true,
+        audio: true,
+      })
+      setLoading(false)
+    } catch (err) {
+      toast({
+        position: 'bottom-left',
+        description: 'Please grant microphone and camera access',
+        status: 'error',
+        isClosable: true,
+      })
+    }
+  }
+
   return loading ? (
     <div className="flex items-center justify-center h-screen">
       <PuffLoader color={colors.teal[500]} size={100} />
     </div>
   ) : (
-    <div>HELLO WORLD</div>
+    <div className="h-screen">HELLO WORLD</div>
   )
 }
 
