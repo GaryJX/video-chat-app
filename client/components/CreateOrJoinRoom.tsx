59167b3851d328e01a5dae64a3acd9252bf13711
Gary Xie <garyxie13@live.com>
2021-07-31 22:18:32 -0400

    Fix z-index

--- client/components/CreateOrJoinRoom.tsx
+++ client/components/CreateOrJoinRoom.tsx
@@ -12,13 +12,12 @@ const CreateOrJoinRoom = () => {
   return (
     <div className="flex items-center justify-center h-screen">
       <div className="flex flex-col p-10 shadow-md rounded-md w-3/4 max-w-sm bg-white dark:bg-gray-700 relative">
-        <div className="absolute top-0 right-0">
+        <div className="absolute top-0 right-0 z-10">
           <ThemeToggle />
         </div>
         <div className={`flex flex-col ${loading ? 'invisible' : ''} `}>
           <div className="mx-auto mb-10 font-bold text-2xl">Video Chat App</div>
-
-          <Button>Create Room</Button>
+          <Button onClick={() => setLoading(true)}>Create New Room</Button>
           <div className={styles.orDivider}>OR</div>
           <TextInput className="mb-5" placeholder="Enter room code" />
           <Button>Join Room</Button>
