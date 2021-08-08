8ff70b81e655e26bf5552e903f7631ee376255eb
Gary Xie <garyxie13@live.com>
2021-08-08 12:29:41 -0400

    Change to form

--- client/components/CreateOrJoinRoom.tsx
+++ client/components/CreateOrJoinRoom.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react'
+import React, { FormEvent, useContext, useState } from 'react'
 import { useRouter } from 'next/dist/client/router'
 import { useToast } from '@chakra-ui/react'
 import { FiClipboard } from 'react-icons/fi'
@@ -27,7 +27,8 @@ const CreateOrJoinRoom: React.FC<CreateOrJoinRoomProps> = ({ roomID }) => {
     router.push(`/room/${newRoomID}`)
   }
 
-  const joinRoomFromHome = () => {
+  const joinRoomFromHome = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
     if (roomIDInput && nameInput) {
       setName(nameInput)
       router.push(`/room/${roomIDInput}`)
@@ -40,7 +41,8 @@ const CreateOrJoinRoom: React.FC<CreateOrJoinRoomProps> = ({ roomID }) => {
     }
   }
 
-  const joinRoomFromRoomPage = () => {
+  const joinRoomFromRoomPage = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
     if (nameInput) {
       setName(nameInput)
     } else {
@@ -70,8 +72,10 @@ const CreateOrJoinRoom: React.FC<CreateOrJoinRoomProps> = ({ roomID }) => {
         <div className="flex flex-col">
           <div className="mx-auto mb-5 font-bold text-2xl">Video Chat App</div>
           {!roomID && (
-            <>
-              <Button onClick={createNewRoom}>Create New Room</Button>
+            <form onSubmit={joinRoomFromHome} className="flex flex-col">
+              <Button type="button" onClick={createNewRoom}>
+                Create New Room
+              </Button>
               <div className={styles.orDivider}>OR</div>
               <label htmlFor="user-name" className="font-bold mb-2">
                 Room ID
@@ -95,11 +99,11 @@ const CreateOrJoinRoom: React.FC<CreateOrJoinRoomProps> = ({ roomID }) => {
                 }
                 className="mb-5"
               />
-              <Button onClick={joinRoomFromHome}>Join Room</Button>
-            </>
+              <Button type="submit">Join Room</Button>
+            </form>
           )}
           {roomID && (
-            <>
+            <form onSubmit={joinRoomFromRoomPage}>
               <label htmlFor="room-id" className="font-bold mb-2">
                 Room ID
               </label>
@@ -115,6 +119,7 @@ const CreateOrJoinRoom: React.FC<CreateOrJoinRoomProps> = ({ roomID }) => {
                   <button
                     className="has-tooltip absolute right-px top-px p-3 focus:outline-none focus:ring-2 focus:ring-teal-500 rounded bg-gray-200 text-teal-600 hover:bg-gray-300"
                     aria-label="Copy to clipboard"
+                    type="button"
                     onClick={copyToClipboard}
                   >
                     <span className="tooltip rounded shadow-lg px-2 py-1 bg-gray-900 dark:bg-gray-100 text-white dark:text-black whitespace-nowrap -top-9 transform -translate-x-1/2">
@@ -136,8 +141,10 @@ const CreateOrJoinRoom: React.FC<CreateOrJoinRoomProps> = ({ roomID }) => {
                 }
                 className="w-full mb-5"
               />
-              <Button onClick={joinRoomFromRoomPage}>Join Room</Button>
-            </>
+              <Button type="submit" className="w-full">
+                Join Room
+              </Button>
+            </form>
           )}
         </div>
       </Container>
